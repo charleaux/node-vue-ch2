339b956e8b0e8486ee9187bd9517113e807c81be
Charles Torry <ctorry@gmail.com>
2018-08-19 18:23:57 -0400

    more

--- public/js/assignment4.js
+++ public/js/assignment4.js
@@ -3,15 +3,19 @@ new Vue({
     data: {
         status: true,
         started: false,
-        noClassArray: ['fatBorder','vipClass'],
+        noClassArray: ['small','vipClass'],
         inputClass: 'small',
         // inputClass: ['fatBorder', 'medium'],
         num4Class: 'small',
-        num4Apply: true
+        num4Apply: true,
+        num5Style: 'red'
     },
     computed: {
         effectClass: function () {
             return {highlight: this.status, shrink: !this.status}
+        },
+        textClass: function() {
+            return JSON.parse(`{"${this.num4Class}": ${this.num4Apply}}`);
         }
     },
     watch: {
