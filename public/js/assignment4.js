2dfe57ebc2df74089351aaafedf4ba3ef5d671ad
Charles Torry <ctorry@gmail.com>
2018-08-19 09:54:29 -0400

    added files

--- public/js/assignment4.js
+++ public/js/assignment4.js
@@ -1,8 +1,30 @@
 new Vue({
     el: '#exercise',
+    data: {
+        highlightStatus: true
+    },
+    computed: {
+        effectClass: function () {
+            console.log('effectClass');
+            return {
+                highlight: this.highlightStatus,
+                shrink: !this.highlightStatus
+            }
+        }
+    },
+    watch: {
+        highlightStatus: function (value) {
+            console.log('highstatus');
+            var vm = this;
+            setTimeout(function () {
+                vm.highlightStatus = !vm.highlightStatus;
+            }, 2000);
+        }
+    },
     methods: {
-        startEffect: function {
-            
+        startEffect: function () {
+            console.log('startEffect');
+            this.highlightStatus = !this.highlightStatus;
         }
     }
 });
